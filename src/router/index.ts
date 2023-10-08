7074e4f92840702110f09205b883697184663585
Leonel Meque <leomeque@gmail.com>
2023-10-08 14:01:32 +0200

    feat: add vue router routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -7,8 +7,12 @@ import PageHomeVue from '@/pages/Home.vue'
 import PageThreadShow from '@/pages/ThreadShow.vue'
 import NotFoundVue from '@/pages/NotFound.vue'
 import ForumPage from '@/pages/ForumPage.vue'
-import sourceData from '../../data.json'
 import CategoryPage from '@/pages/CategoryPage.vue'
+import ProfilePage from '@/pages/ProfilePage.vue'
+import ThreadCreate from '@/pages/ThreadCreate.vue'
+import ThreadEdit from '@/pages/ThreadEdit.vue'
+import { findById } from '@/helpers'
+import { useStore } from 'vuex'
 
 const routes: RouterOptions['routes'] = [
   {
@@ -23,14 +27,31 @@ const routes: RouterOptions['routes'] = [
     props: true
   },
   {
+    path: '/me',
+    name: 'Profile',
+    component: ProfilePage,
+    props: { edit: false },
+    meta: { toTop: true, smoothScroll: true }
+  },
+  {
+    path: '/me/edit',
+    name: 'ProfileEdit',
+    component: ProfilePage,
+    props: { edit: true }
+  },
+  {
     path: '/thread/:id',
     name: 'ThreadShow',
     component: PageThreadShow,
     props: true,
     beforeEnter(to, _from, next) {
-      const threadExists = sourceData.threads.find(
-        (t) => t.id === to.params.id
+      const store = useStore()
+
+      const threadExists = findById(
+        store.state.threads,
+        (to as any)?.params?.id
       )
+
       if (threadExists) return next()
       else
         next({
@@ -40,6 +61,18 @@ const routes: RouterOptions['routes'] = [
           hash: to.hash
         })
     }
+  },
+  {
+    path: '/forum/:forumId/thread/create',
+    name: 'ThreadCreate',
+    component: ThreadCreate,
+    props: true
+  },
+  {
+    path: '/thread/:id/edit',
+    name: 'ThreadEdit',
+    component: ThreadEdit,
+    props: true
   },
   {
     path: '/forum/:id',
@@ -56,7 +89,15 @@ const routes: RouterOptions['routes'] = [
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  scrollBehavior(to) {
+    const scroll: { [key: string]: any } = {}
+
+    if (to.meta.toTop) scroll.to = 0
+    if (to.meta.smoothScroll) scroll.behavior = 'smooth'
+
+    return scroll
+  }
 })
 
 export default router
